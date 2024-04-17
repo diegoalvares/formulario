bceb92113fb54c256b1d964dedef6642ab7367b9
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2024-04-17 14:46:39 -0300

    atualizando.

--- funcoes.js
+++ funcoes.js
@@ -1,5 +1,23 @@
-const olho = document.getElementById('olho')
+const nome = document.getElementById('nome');
+const s_nome = document.getElementById('sobrenome');
+const rua = document.getElementById('rua');
+const num = document.getElementById('num');
+const cidade = document.getElementById('cidade');
+const uf = document.getElementById('estado');
+const tel = document.getElementById('tel');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
 
-olho.addEventListener('click', function(){
-    
+const cadastrar = document.getElementById('cadastrar')
+
+cadastrar.addEventListener('click', function(){
+    if(nome.value == '' || s_nome.value == '' || rua.value == '' ||
+        num.value == '' || cidade.value == '' || uf.value == '------' || tel.value == '' || email.value == '' || senha.value == ''){
+            
+        console.log('existem campos vazios!')
+    }
+
+    else{
+        console.logo('cadastro efetuado com sucesso!')
+    }
 })
