a12990051e192d7b091f5489cd60623a579063df
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2024-04-17 21:17:30 -0300

    atualiando.

--- funcoes.js
+++ funcoes.js
@@ -11,10 +11,10 @@ const senha = document.getElementById('senha');
 const cadastrar = document.getElementById('cadastrar')
 
 cadastrar.addEventListener('click', function(){
-    if(nome.value == '' || s_nome.value == '' || rua.value == '' || num.value == '' || cidade.value == '' || uf.value == '------' || tel.value == '' ||
-    email.value == '' || senha.value == ''){
-        alert('Existem campos vazios!')} 
-    
+    if(nome.value == '' || s_nome.value == '' || rua.value == '' || num.value == '' || cidade.value == '' || uf.value == 'nulo' || tel.value == '' ||
+    email.value == '' || senha.value == '')
+        alert('Existem campos não preenchidos!');
+   
     else{
         alert('Seu cadastro foi realizado com sucesso!')
     }
