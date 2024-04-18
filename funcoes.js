2a6531a8a70784159375e5f336257f4c22787113
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2024-04-18 14:58:16 -0300

    atualizando.

--- funcoes.js
+++ funcoes.js
@@ -7,17 +7,41 @@ const uf = document.getElementById('estado');
 const tel = document.getElementById('cel');
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
-
+const erro = document.getElementById('erro');
+const finalizar = document.getElementById('finalizar')
 const cadastrar = document.getElementById('cadastrar')
+const olho = document.getElementById('olho')
+const img_olho = document.getElementById('img_olho')
+const ok = document.getElementById('ok')
+
+ok.addEventListener('click', function(){
+    finalizar.style.display='none';
+})
+
+olho.addEventListener('click', function(e){
+    e.preventDefault(); 
+    if (senha.getAttribute('type') == 'password') {
+        senha.setAttribute('type', 'text');
+        img_olho.setAttribute('src', 'icones/olho-aberto.png'); 
+    } else {
+        img_olho.setAttribute('src', 'icones/olho-fechado.png'); 
+        senha.setAttribute('type', 'password')
+    }
+})
+
 
 cadastrar.addEventListener('click', function(e){
-    if(nome.value == '' || s_nome.value == '' || rua.value == '' || num.value == '' || cidade.value == '' || uf.value == 'nulo' || tel.value == '' ||
-    email.value == '' || senha.value == ''){
+    if(nome.value == '' || s_nome.value == '' || rua.value == '' || num.value == '' || cidade.value == '' || uf.value == 'nulo' || tel.value == '' || email.value == '' || senha.value == ''){
+        erro.style.display='block'
         e.preventDefault();
-        alert('Existem campos não preenchidos!');
+
+        function apagar(){
+            erro.style.display='none';
+        }
+        setTimeout(apagar, '3000')
     }
    
     else{
-        alert('Cadastro efetuado com sucesso!')
+        finalizar.style.display='block'
     }
 })
